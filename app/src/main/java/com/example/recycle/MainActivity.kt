f8dd45c270862aa2d5a2e957b9bd03f1bb0e34ff
Kunal <kunalmehrotra2001@gmail.com>
2022-06-13 02:46:36 +0530

    Added bottom nav bar

--- app/src/main/java/com/example/recycle/MainActivity.kt
+++ app/src/main/java/com/example/recycle/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.recycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.itemIconTintList=null
+        binding.bottomNavigationView.background=null
     }
 }
