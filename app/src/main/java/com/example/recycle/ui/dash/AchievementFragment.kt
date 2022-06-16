9fd7e0103f636d8277bc2214913d0b7c998a0c69
Sandeep Gupta <guptasg300@gmail.com>
2022-06-16 18:01:10 +0530

    achievement and home ui

--- app/src/main/java/com/example/recycle/ui/dash/AchievementFragment.kt
+++ app/src/main/java/com/example/recycle/ui/dash/AchievementFragment.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.recycle.R
+import com.example.recycle.databinding.FragmentAchievementBinding
+import com.example.recycle.model.LeaderboardUser
 
 class AchievementFragment : Fragment() {
 
+    lateinit var binding: FragmentAchievementBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val userList = mutableListOf<LeaderboardUser>()
+
+        userList.add(LeaderboardUser("Sandeep",100,R.drawable.avtaar))
+        userList.add(LeaderboardUser("Ananya",50,R.drawable.avtaar))
+        userList.add(LeaderboardUser("Suyash",40,R.drawable.avtaar))
+        userList.add(LeaderboardUser("Ayush",10,R.drawable.avtaar))
+        userList.add(LeaderboardUser("Kunal",1,R.drawable.avtaar))
+
+
 
     }
 
@@ -18,8 +32,9 @@ class AchievementFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_achievement, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_achievement,container,false)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/recycle/ui/dash/HomeFragment.kt
+++ app/src/main/java/com/example/recycle/ui/dash/HomeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.recycle.R
+import com.example.recycle.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +22,9 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_home, container, false)
+
+        return binding.root
     }
 
 }
