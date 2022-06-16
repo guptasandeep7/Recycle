807a809fa5a9213d2640e0cf9f1dd7accf21fa02
Kunal <kunalmehrotra2001@gmail.com>
2022-06-16 22:58:07 +0530

    Added expandable cardview in home page

--- app/src/main/java/com/example/recycle/Activity/AuthAcitivity.kt
+++ app/src/main/java/com/example/recycle/Activity/AuthAcitivity.kt
@@ -1,12 +1,23 @@
 package com.example.recycle.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.recycle.R
+import com.example.recycle.Repo.Datastore
+import kotlinx.coroutines.launch
 
 class AuthAcitivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth_acitivity)
+        var datastore=Datastore(this)
+        lifecycleScope.launch {
+            if (datastore.isLogin() == true) {
+                var intent=Intent(this@AuthAcitivity,MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
