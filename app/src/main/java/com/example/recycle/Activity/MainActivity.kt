943469825721c9d37d8fd4e5d0728f39debdfbcf
Kunal <kunalmehrotra2001@gmail.com>
2022-06-17 18:29:54 +0530

    Fixed Bugs

--- app/src/main/java/com/example/recycle/Activity/MainActivity.kt
+++ app/src/main/java/com/example/recycle/Activity/MainActivity.kt
@@ -18,9 +18,9 @@ import com.example.recycle.Ui.dash.ObjectDetection
 import com.example.recycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    companion object{lateinit var image: Bitmap}
+
     lateinit var binding: ActivityMainBinding
-    val imageSize = 224
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,44 +33,9 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_dash_container) as NavHostFragment
         val navController: NavController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
-        binding.floatingButtonTeachersSide.setOnClickListener{
-            if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-                } else {
-                    TODO("VERSION.SDK_INT < M")
-                }
-            ) {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(cameraIntent, 1)
-
-            } else {
-                //Request camera permission if we don't have it.
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
-                }
-            }
+        binding.floatingButtonTeachersSide.setOnClickListener {
+            var intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
         }
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            image = data!!.extras!!["data"] as Bitmap
-            val dimension = Math.min(image!!.width, image!!.height)
-            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
-            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
-            val fragmentManager =this.supportFragmentManager
-            val fragmentTransaction = fragmentManager?.beginTransaction()
-            fragmentTransaction?.replace(R.id.fragment_dash_container,ObjectDetection())
-            fragmentTransaction?.addToBackStack(null)
-            fragmentTransaction?.commitAllowingStateLoss()
-
-        }
-
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onPostCreate(savedInstanceState, persistentState)
-
-    }
-
 }
